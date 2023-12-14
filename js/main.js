866775cea00042663869562b0c34be2bad787dfc
Kovalchuk-Alexandr <mo250568kaa@gmail.com>
2023-12-14 12:02:13 +0200

    mobile release

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ const bgGrayPath = "./../img/header/bg-gray.jpg";
 for (let chk of radio) {
     // console.log(btn);
     chk.addEventListener("click", (e) => {
-        console.log("---- Radio ----");
-        console.log(chk.value);
+        // console.log("---- Radio ----");
+        // console.log(chk.value);
 
         const bg = document.querySelector(".header");
         console.log(bg.style.backgroundImage);
@@ -59,6 +59,11 @@ for (let chk of radio) {
 // Клик по кнопке с подсказками
 for (let btn of infoBtns) {
     // console.log(btn);
+
+    // Закрываем подксказки (Hints) на других кнопках, если понадобится
+    closeHints();
+
+    // Показываем подсказку
     btn.addEventListener("click", showHint);
 }
 
@@ -69,7 +74,7 @@ function showHint(e) {
 
     // Запрещаем всплытие события клика наверх при клике по подсказке
     e.stopPropagation();
-    console.log(this.parentNode);
+    // console.log(this.parentNode);
 
     if (this.parentNode.querySelector(".info-hint")) {
         this.parentNode.querySelector(".info-hint").classList.toggle("none");
@@ -81,7 +86,7 @@ function showHint(e) {
 // Закрываем подсказки при клике по всему документу
 document.addEventListener("click", closeHints);
 
-function closeHints(params) {
+function closeHints() {
     for (const hint of infoHints) {
         hint.classList.add("none");
     }
@@ -92,42 +97,38 @@ for (const hint of infoHints) {
     hint.addEventListener("click", (e) => e.stopPropagation());
 }
 
-// Переключение табов в продуктах
-for (let btn of tabBtns) {
-    btn.addEventListener("click", () => {
+// Переключение табов в продуктах (Мой начальный вариант )
+// for (let btn of tabBtns) {
+//     btn.addEventListener("click", () => {
 
-        console.log(btn);
+//         console.log(btn);
        
-        // По клику делаем активной таб
-        if (!btn.classList.contains("tab-controls__btn--active")) {
-            document
-                .querySelector(".tab-controls__btn--active")
-                .classList.remove("tab-controls__btn--active");
-            btn.classList.add("tab-controls__btn--active");
-        }
-    });
-}
+//         // По клику делаем активной таб
+//         if (!btn.classList.contains("tab-controls__btn--active")) {
+//             document
+//                 .querySelector(".tab-controls__btn--active")
+//                 .classList.remove("tab-controls__btn--active");
+//             btn.classList.add("tab-controls__btn--active");
+//         }
+//     });
+// }
  
 function showHide(name) {
-    console.log("Hello in showHide!");
-    console.log(name);
-    console.log(name.getAttribute("title"));
-
-    // name.classList[0]: короткая запись выбора первого элемента 'name.classList.value'
+     // name.classList[0]: короткая запись выбора первого элемента 'name.classList.value'
     let hint = "";
 
-    switch (name.classList[0]) {
-        case "header__dot-vase":
+    switch (name.classList[1]) {
+        case "header__dot--vase":
             hint = vase.classList.contains("visible") ? "Show" : "Hide";
             name.setAttribute("title", hint);
             vase.classList.toggle("visible");
             break;
-        case "header__dot-item":
+        case "header__dot--item":
             hint = item.classList.contains("visible") ? "Show" : "Hide";
             name.setAttribute("title", hint);
             item.classList.toggle("visible");
             break;
-        case "header__dot-pot":
+        case "header__dot--pot":
             hint = pot.classList.contains("visible") ? "Show" : "Hide";
             name.setAttribute("title", hint);
             pot.classList.toggle("visible");
@@ -141,9 +142,9 @@ function showHide(name) {
 // Swiper slider
 const swiper = new Swiper(".swiper", {
     // Optional parameters
-    cssMode: true,
+    // cssMode: true,   // отменяет прокрутку смахиванием мыши, только через кнопки
     direction: "horizontal",
-    loop: true,
+    //loop: true,
     freeMode: true,
 
     slidesPerView: 1,
@@ -157,7 +158,7 @@ const swiper = new Swiper(".swiper", {
         },
         768: {
             slidesPerView: 3,
-            spaceBetween: 30,
+            spaceBetween: 25,
         },
         1024: {
             slidesPerView: 4,
@@ -195,3 +196,74 @@ const swiper = new Swiper(".swiper", {
         el: ".swiper-scrollbar",
     },
 });
+
+
+// Tabs (str.95)
+const tabsBtns = document.querySelectorAll('[data-tab]');
+const tabsProducts = document.querySelectorAll('[data-tab-value]');
+
+// console.log(tabsBtns);
+// console.log(tabsProducts);
+// console.log(tabsProducts.length);
+
+for (const btn of tabsBtns) {
+    // console.log(btn);
+
+    btn.addEventListener('click', function() {
+        console.log(this);
+
+        // Убираем активный класс у всех кнопок
+        for (const btn of tabsBtns) {
+            btn.classList.remove("tab-controls__btn--active");
+        }
+
+        // Добавляем активный класс к текущей кнопке
+        this.classList.add("tab-controls__btn--active");
+
+        // Получаем значение выбранной категории товаров
+        // 'this' - элемент по которому кликнули
+        // 'dataset.tab' - значение атрибута 'tab'
+        console.log(this.dataset.tab);
+        let items = 0;
+        // Отображаем нужные товары, скрываем не нужные
+        for (const product of tabsProducts ) {
+            // console.log(product.dataset.tabValue);
+            if (this.dataset.tab == 'all') {
+                product.classList.remove('none');
+                items++;
+            }
+
+            // если продукт совпадает с выбранным в табе - показываем
+            // иначе - скрываем
+            else {
+                if (product.dataset.tabValue === this.dataset.tab) {
+                    product.classList.remove('none');
+                    items++;
+                } else
+                    product.classList.add('none')
+            }
+        }
+        
+        console.log(`In "${this.dataset.tab}" tab: ${items} item(s)`);
+        
+        // Update Swiper после использования таба
+        swiper.update();
+        swiper.updateSlides('#open-mobie-nav-btn');
+    })
+}
+
+
+// --------- Mobile nav ---------------------
+const mobileNavOpenBtn = document.querySelector("#open-mobie-nav-btn");
+const mobileNavCloseBtn = document.querySelector("#close-mobie-nav-btn");
+const mobileNav = document.querySelector("#mobile-nav");
+
+
+mobileNavOpenBtn.onclick = function () {
+    mobileNav.classList.add("mobile-nav-wrapper--open");
+}
+
+mobileNavCloseBtn.onclick = function () {
+    mobileNav.classList.remove("mobile-nav-wrapper--open");
+}
+
